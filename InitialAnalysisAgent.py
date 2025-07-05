1113b55e293b0a9f1f19e3c95de41ae26d30bd7c
Shashwat M <65341284+ShashwatM3@users.noreply.github.com>
2025-07-05 23:55:28 +0400

    Add files via upload

---
+++ InitialAnalysisAgent.py
@@ -0,0 +1,37 @@
+from rich import *
+from pydantic import BaseModel
+from rich.panel import Panel
+from agents import Agent, Runner, function_tool
+
+class analysisOutput(BaseModel):
+  thoughts: str
+  initial_queries: list[str]
+
+analysis_agent = Agent(
+  name="Analysis Agent",
+  instructions="""
+You are a research planning agent. Your goal is to generate a concise, strategic research direction that will guide the entire deep research process.
+
+Given a user's high-level research query, your task is to:
+1. Restate the goal clearly.
+2. Identify what angles or aspects the research will focus on.
+3. Specify trusted source types to prioritize.
+4. Anticipate any major challenges in finding or verifying information.
+5. Explain how those challenges will be addressed.
+6. Finally, generate 3 focused search queries that the system will use to retrieve information.
+
+Return the output in the following format:
+
+Thoughts:
+A 4–5 sentence paragraph summarizing all key aspects above in plain language. This paragraph defines the overall research direction and will be used to guide subsequent agent behaviors.
+
+Generated Search Queries: [<query 1>, <query 2>, <query 3>]
+These search queries must focus on providing the best information to contribute to the research direction, and when answered, should form end-to-end deep research content.
+""",
+  output_type=analysisOutput
+)
+
+async def get_initial_analysis(userQuery):
+  result = await Runner.run(analysis_agent, userQuery)
+  analysis = result.final_output
+  return analysis
---
+++ SynthesisAgent.py
@@ -0,0 +1,20 @@
+from agents import Agent, Runner
+
+synthesis_agent = Agent(
+    name="Synthesis Agent",
+    instructions=(
+    "You are a research report writer. You will receive an original query followed by multiple summaries "
+    "of web search results. Your task is to create a comprehensive report that addresses the original query "
+    "by combining the information from the search results into a coherent whole. "
+    "The report should be well-structured, informative, and directly answer the original query. "
+    "Focus on providing actionable insights and practical information. "
+    "Aim for up to 5-6 pages with clear sections and a conclusion. "
+    "Important: Use markdown formatting with headings and subheadings. Have a table of contents in the beginning of the report that links to each section."
+    "Try and include in-text citations to the sources used to create the report with a source list at the end of the report. Include all websites provided"
+    ),
+    model="gpt-4o",
+)
+
+async def getSynthesis(combined_summary):
+  result = await Runner.run(synthesis_agent, combined_summary)
+  return result.final_output
